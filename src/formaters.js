150fa8c321195b29e8bdf7ded9ee7bbd34375b43
al-ov73 <klever1987@yandex.ru>
2024-02-28 00:31:24 +0400

    lint comments

--- src/formaters.js
+++ src/formaters.js
@@ -1,32 +1,32 @@
-import isObject from "./utils.js";
+import isObject from './utils.js';
 
 const replacer = ' ';
 
-const strObject = (data, replacer, depth) => {
-  let result = '';
+const strObject = (obj, depth) => {
+  let resultValue = '';
   depth -= 1;
 
-  const iter = (data, result, depth) => {
-    const indent = replacer.repeat(depth * 4);
+  const iter = (data, result, dep) => {
+    const indent = replacer.repeat(dep * 4);
     if (isObject(data)) {
-      for (let key of Object.keys(data)) {
+      Object.keys(data).forEach((key) => {
         if (isObject(data[key])) {
-          const value = iter(data[key], result, depth + 1);
-          result += `    ${key}: {\n`
-          result += `    ${value}`
-          result += `    }\n`
+          const value = iter(data[key], result, dep + 1);
+          result += `    ${key}: {\n`;
+          result += `    ${value}`;
+          result += '    }\n';
         } else {
-          result += `    ${key}: ${data[key]}\n`
+          result += `    ${key}: ${data[key]}\n`;
         }
-      }
+      });
     } else {
-    result += ` ${indent}${data}\n`;
-  }
-  return result;
-  }
-  result = iter(data, result, depth);
-  return result;
-}
+      result += ` ${indent}${data}\n`;
+    }
+    return result;
+  };
+  resultValue = iter(obj, resultValue, depth);
+  return resultValue;
+};
 
 const formatToStylish = (diff, depth = 1, result = '') => {
   const allKeys = Object.keys(diff).reduce((acc, item) => {
@@ -42,17 +42,17 @@ const formatToStylish = (diff, depth = 1, result = '') => {
 
   allKeys.sort().forEach((key) => {
     if (diff[key][0] === 'removed') {
-      const value = diff[key][1]
-      result += `${indent}- ${key}:${strObject(value, replacer, depth)}`;
+      const value = diff[key][1];
+      result += `${indent}- ${key}:${strObject(value, depth)}`;
     } else if (diff[key][0] === 'added') {
-      const value = diff[key][1]
-      result += `${indent}+ ${key}:${strObject(value, replacer, depth)}`;
+      const value = diff[key][1];
+      result += `${indent}+ ${key}:${strObject(value, depth)}`;
     } else if (diff[key][0] === 'not changed') {
-      const value = diff[key][1]
-      result += `${indent}  ${key}:${strObject(value, replacer, depth)}`;
+      const value = diff[key][1];
+      result += `${indent}  ${key}:${strObject(value, depth)}`;
     } else if (diff[key][0] === 'changed value') {
-      result += `${indent}- ${key}:${strObject(diff[key][1], replacer, depth)}`;
-      result += `${indent}+ ${key}:${strObject(diff[key][2], replacer, depth)}`;
+      result += `${indent}- ${key}:${strObject(diff[key][1], depth)}`;
+      result += `${indent}+ ${key}:${strObject(diff[key][2], depth)}`;
     } else if (diff[key][0] === 'changed object') {
       depth += 1;
       result += `${indent}  ${key}: {\n`;
