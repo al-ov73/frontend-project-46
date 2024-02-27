4271d033119748ea93763eaa22d28b05ce73b34c
al-ov73 <klever1987@yandex.ru>
2024-02-27 15:03:15 +0300

    modif recurse formater

--- src/formaters.js
+++ src/formaters.js
@@ -1,31 +1,35 @@
-const formatToStylish = (diff) => {
-  // console.log(diff);
+const space = ' '
+
+const formatToStylish = (diff, depth=1, result = '') => {
   const allKeys = Object.keys(diff).reduce((acc, item) => {
     if (acc.includes(item)) {
       return acc;
     }
     return [...acc, item];
   }, []);
-  let result = '{\n';
+  const indent = space.repeat(depth*4 - 2);
+  if (depth === 1) {
+    result += '{\n'
+  }
   allKeys.sort().forEach((key) => {
     if (diff[key][0] === 'removed') {
-      result += `  - ${key}: ${diff[key][1]}\n`;
+      result += `${indent}- ${key}: ${diff[key][1]}\n`;
     } else if (diff[key][0] === 'added') {
-      result += `  + ${key}: ${diff[key][1]}\n`;
+      result += `${indent}+ ${key}: ${diff[key][1]}\n`;
     } else if (diff[key][0] === 'not changed') {
-      result += `    ${key}: ${diff[key][1]}\n`;
+      result += `${indent}  ${key}: ${diff[key][1]}\n`;
     } else if (diff[key][0] === 'changed value') {
-      result += `  - ${key}: ${diff[key][1]}\n`;
-      result += `  + ${key}: ${diff[key][2]}\n`;
+      result += `${indent}- ${key}: ${diff[key][1]}\n`;
+      result += `${indent}+ ${key}: ${diff[key][2]}\n`;
     } else if (diff[key][0] === 'changed object') {
-      // console.log('will create stylish11:', diff[key]);
-
-      result += `    ${key}:\n`;
-      result += `        ${formatToStylish(diff[key][1])}\n`;
-      // result += `  + ${key}: 1${formatToStylish(diff[key][2])}\n`;
+      depth += 1;
+      result += `${indent}  ${key}:  {\n`;  
+      result += formatToStylish(diff[key][1], depth);
     }
   });
-  result += '}';
+  if (depth === 1) {
+    result += '}'
+  }
   return result;
 };
 
